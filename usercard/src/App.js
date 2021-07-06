d78b058765faad2acd836dbddea19f244d534657
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-07-06 16:25:22 -0700

    initial commit/ setUp page

---
+++ usercard/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        <code>Github User Card</code>  
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
